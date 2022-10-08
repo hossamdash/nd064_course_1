a45374889e1f73d1fe7425d1bb77a9d651cb8e3e
Hossam Mostafa <hossamdash2@gmail.com>
2022-10-08 12:53:58 +0200

    Project techtrends first commit

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,70 +1,134 @@
 import sqlite3
 
-from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
-from werkzeug.exceptions import abort
+
+from flask import (
+    Flask,
+    render_template,
+    json,
+    request,
+    url_for,
+    redirect,
+    flash,
+)
+
+
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+global connection_count
+connection_count = 0
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
+    global connection_count
+    connection = sqlite3.connect("database.db")
+    connection_count += 1
     connection.row_factory = sqlite3.Row
     return connection
+
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = connection.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
     connection.close()
     return post
 
+
 # Define the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+app.config["SECRET_KEY"] = "your secret key"
 
-# Define the main route of the web application 
-@app.route('/')
+
+# Define the main route of the web application
+@app.route("/")
 def index():
     connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
+    posts = connection.execute("SELECT * FROM posts").fetchall()
     connection.close()
-    return render_template('index.html', posts=posts)
+    return render_template("index.html", posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
-@app.route('/<int:post_id>')
+@app.route("/<int:post_id>")
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.debug("a non-existing post was retrieved")
+        return render_template("404.html"), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.debug('a post with title "{}" was retrieved'.format(post[2]))
+        return render_template("post.html", post=post)
+
 
 # Define the About Us page
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    app.logger.debug("about page was accessed")
+    return render_template("about.html")
 
-# Define the post creation functionality 
-@app.route('/create', methods=('GET', 'POST'))
+
+# Define the post creation functionality
+@app.route("/create", methods=("GET", "POST"))
 def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
 
         if not title:
-            flash('Title is required!')
+            flash("Title is required!")
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute(
+                "INSERT INTO posts (title, content) VALUES (?, ?)", (title, content)
+            )
+            app.logger.debug('a new post with title "{}" was created'.format(title))
             connection.commit()
             connection.close()
 
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-    return render_template('create.html')
+    return render_template("create.html")
+
+
+# health check
+@app.route("/healthz")
+def healthz():
+    app.logger.info("Status check successfull")
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype="application/json",
+    )
+    return response
+
+
+# metrics
+@app.route("/metrics")
+def metrics():
+    app.logger.info("Metrics check successfull")
+    connection = get_db_connection()
+    post_count_sequence = connection.execute("SELECT count(*) FROM posts").fetchone()
+    post_count = [post_count_sequence[0] for post_count in post_count_sequence]
+    response = app.response_class(
+        response=json.dumps(
+            {
+                "db_connection_count": connection_count,
+                "post_count": post_count,
+            }
+        ),
+        status=200,
+        mimetype="application/json",
+    )
+    return response
+
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    app.run(host="0.0.0.0", port="3111")
